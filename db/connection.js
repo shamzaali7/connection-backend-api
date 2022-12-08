2f22715dad5f48a6b9754e1632a4ae1be626af1a
CaseyDragon <cndragon83@gmail.com>
2022-12-08 10:17:29 -0500

    connection file set up

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose")
 require("dotenv").config();
 
+mongoose.Promise = Promise;
+
 const mongoURI= process.env.DATABASE_URL;
 
 mongoose
     .connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((instance) => 
+    console.log(`connected to db: ${instance.connections[0].name}`, error)
+    )
+    .catch((error) => console.log('Connection failed!', error));
     
+
+module.exports = mongoose
