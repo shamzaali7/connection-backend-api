ebb2603b47293f139f2b37f514fa54f57b8a3a97
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-09 14:24:43 -0500

    DB_URL

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ mongoose.Promise = Promise;
 let mongoURI = "";
 
 if (process.env.NODE_ENV === "production"){
-    mongoURI = process.env.DATABASE_URL
+    mongoURI = process.env.DB_URL
 }else{
     mongoURI = "mongodb://localhost/connection"
 }
