7ac6b83d35e72b63f5c45b1a957eee9086820e88
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-09 14:28:11 -0500

    c  capital

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const Contact = require("../models/Contact")
+const Contact = require("../models/contact")
 
 router.get("/", async (req, res, next) => {
     try{
