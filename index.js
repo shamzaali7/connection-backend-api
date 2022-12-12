6a16951fac7aa4c0a862ac51073b51bc05199079
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-12 14:39:48 -0500

    cors changes

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app = express();
 app.set("port", process.env.PORT || 4000);
 const cors = require("cors");
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use(cors({
-    origin: "*",
+    origin : "*",
 	methods: "GET, POST, PATCH, PUT, DELETE"
 }))
 
