a5df3e0031e379faf9fb1ce3e2a13e46d84452fc
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-12 10:28:48 -0500

    cors proxy test2

--- db/connection.js
+++ db/connection.js
@@ -6,15 +6,6 @@ let host = process.env.Host || "0.0.0.0";
 let port = process.env.PORT || 8080;
 let mongoURI = "";
 
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
-
 if (process.env.NODE_ENV === "production"){
     mongoURI = process.env.DB_URL
 }else{
