b48a3e501c46e3a6fbfbece12074a90a65dbe3ec
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-12 10:20:26 -0500

    cors proxy test

--- db/connection.js
+++ db/connection.js
@@ -2,8 +2,18 @@ const mongoose = require("mongoose")
 require("dotenv").config();
 mongoose.set('strictQuery', false)
 mongoose.Promise = Promise;
-
+let host = process.env.Host || "0.0.0.0";
+let port = process.env.PORT || 8080;
 let mongoURI = "";
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
 
 if (process.env.NODE_ENV === "production"){
     mongoURI = process.env.DB_URL
