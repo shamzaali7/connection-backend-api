220230e2901742ed488756d1b887a27fc16b7354
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-12 11:53:01 -0500

    setHeader

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(cors({
 }))
 
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
 	next();
 })
 
